cfa27a2f27166d758a7568d86ef58479602a9733
Zeglius <33781398+Zeglius@users.noreply.github.com>
2023-12-06 19:38:58 +0100

    fix: move logout button to a topbar to avoid visual clutter

--- app/src/main/java/com/zeglius/my_firebase_app/TravelListActivity.kt
+++ app/src/main/java/com/zeglius/my_firebase_app/TravelListActivity.kt
@@ -7,23 +7,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Logout
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
@@ -124,7 +126,7 @@ private fun TravelListContent(onSignOut: () -> Unit) {
 
 
     Scaffold(
-        floatingActionButton = {
+        /*floatingActionButton = {
             FloatingActionButton(
                 onClick = onSignOut,
                 containerColor = MaterialTheme.colorScheme.primary,
@@ -138,6 +140,17 @@ private fun TravelListContent(onSignOut: () -> Unit) {
                     Text(text = stringResource(R.string.log_out))
                 }
             }
+        },*/
+        topBar = {
+            TopAppBar(
+                title = { TextHeader(text = stringResource(R.string.my_travel_list)) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(),
+                actions = {
+                    IconButton(onClick = onSignOut) {
+                        Icon(Icons.Default.Logout, contentDescription = "Log out")
+                    }
+                }
+            )
         }
     ) { paddingValues ->
         Column(
@@ -145,9 +158,9 @@ private fun TravelListContent(onSignOut: () -> Unit) {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
-            Spacer(modifier = Modifier.height(20.dp))
-            TextHeader(text = stringResource(R.string.my_travel_list))
-            Spacer(modifier = Modifier.height(20.dp))
+            /*    Spacer(modifier = Modifier.height(20.dp))
+
+                Spacer(modifier = Modifier.height(20.dp))*/
 
 
             CreateViajeDialog(
@@ -159,12 +172,6 @@ private fun TravelListContent(onSignOut: () -> Unit) {
                 verticalArrangement = Arrangement.spacedBy(8.dp),
                 modifier = Modifier.padding(10.dp),
             ) {
-                items(viajesList) { viaje ->
-                    ViajeItem(viaje) { viaje ->
-                        selectedViaje = viaje; isCreateViajeVisible = true
-                    }
-                }
-
                 item {
                     Button(
                         onClick = { isCreateViajeVisible = true },
@@ -174,6 +181,13 @@ private fun TravelListContent(onSignOut: () -> Unit) {
                     }
 
                 }
+
+                items(viajesList) { viaje ->
+                    ViajeItem(viaje) { viaje ->
+                        selectedViaje = viaje; isCreateViajeVisible = true
+                    }
+                }
+
             }
         }
     }
